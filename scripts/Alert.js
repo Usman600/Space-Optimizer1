1a072d165914cd12678d3bbaedd0fa2d88264729
Usman600 <muhmmadusman677@gmail.com>
2024-05-17 13:41:16 -0700

    function for show Error Alert

--- scripts/Alert.js
+++ scripts/Alert.js
@@ -6,7 +6,13 @@
     }, 3000);
 }
 
-
+function showErrorAlert() {
+    var errorAlert = document.getElementById('ContentPlaceHolder1_errorAlert');
+    errorAlert.style.display = 'flex';
+    setTimeout(function () {
+        dismissAlert('ContentPlaceHolder1_errorAlert', errorAlert.querySelector('.okBtn'));
+    }, 3000);
+}
 
 function dismissAlert(alertId, button) {
     var alertElement = document.getElementById(alertId);
