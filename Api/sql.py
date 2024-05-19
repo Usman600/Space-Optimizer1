a012a9680ffc62c565977a77dbcf9ad43c2d5a05
Filzawasim <124429531+Filzawasim@users.noreply.github.com>
2024-05-19 19:26:57 +0500

    Update sql.py

--- Api/sql.py
+++ Api/sql.py
@@ -19,3 +19,13 @@ IDs = '32 '
 # Call the stored procedure with parameters
 cursor.execute(f"EXEC SP_User_Shape_Query @P_UserDetailID_fk=?, @P_Index=?, @P_ContainerID=?, @P_IDs=?", UserDetailID_fk, Index, ContainerID, IDs)
 
+# Fetch the results if needed
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+image = rows[0][5]
+base64_data = '0x'+image
+new_image = Image.new("RGB", base64_data.size, (255, 255, 255))
+new_image.paste(image, mask=base64_data.split()[3])
+image_array = np.array(new_image)
+print(new_image)
