f6ee9671f4d92ce12670d07dc6069789a7571b50
Filzawasim <124429531+Filzawasim@users.noreply.github.com>
2024-05-22 14:11:44 +0500

    Update SessionObject.vb

--- App_Code/SessionObject.vb
+++ App_Code/SessionObject.vb
@@ -1,10 +1,2 @@
 ﻿Imports Microsoft.VisualBasic
 
-Public Class SessionObject
-    Public Structure AdminObj
-        Public UserID As Integer
-        Public UserName As String
-        Public SessionID As String
-        Public model As Model
-    End Structure
-End Class
