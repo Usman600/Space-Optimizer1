f771bb804dd4eb63899be495843a17249a727534
Filzawasim <124429531+Filzawasim@users.noreply.github.com>
2024-05-22 18:45:26 +0500

    Update ViewSwitcher.ascx.vb

--- ViewSwitcher.ascx.vb
+++ ViewSwitcher.ascx.vb
@@ -9,5 +9,27 @@ Public Partial Class ViewSwitcher
     Protected Property AlternateView As String
     Protected Property SwitchUrl As String
 
-   
+    Protected Sub Page_Load(sender As Object, e As EventArgs)
+        ' Determine current view
+        Dim isMobile = WebFormsFriendlyUrlResolver.IsMobileView(New HttpContextWrapper(Context))
+        CurrentView = If(isMobile, "Mobile", "Desktop")
+
+        ' Determine alternate view
+        AlternateView = If(isMobile, "Desktop", "Mobile")
+
+        ' Create switch URL from the route, e.g. ~/__FriendlyUrls_SwitchView/Mobile?ReturnUrl=/Page
+        Dim switchViewRouteName = "AspNet.FriendlyUrls.SwitchView"
+        Dim switchViewRoute = RouteTable.Routes(switchViewRouteName)
+        If switchViewRoute Is Nothing Then
+            ' Friendly URLs is not enabled or the name of the switch view route is out of sync
+            Me.Visible = False
+            Return
+        End If
+        Dim url = GetRouteUrl(switchViewRouteName, New With {
+            .view = AlternateView,
+            .__FriendlyUrls_SwitchViews = True
+        })
+        url += "?ReturnUrl=" & HttpUtility.UrlEncode(Request.RawUrl)
+        SwitchUrl = url
+    End Sub
 End Class
