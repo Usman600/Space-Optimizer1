aab1e9047f2614bde29ae5303ca395a23d163dc8
Filzawasim <124429531+Filzawasim@users.noreply.github.com>
2024-05-22 18:41:29 +0500

    Update IdentityModels.vb
    updated some code.

--- App_Code/IdentityModels.vb
+++ App_Code/IdentityModels.vb
@@ -16,6 +16,32 @@ Public Class ApplicationDbContext
 End Class
 
 #Region "Helpers"
+Public Class UserManager
+    Inherits UserManager(Of ApplicationUser)
+    Public Sub New()
+        MyBase.New(New UserStore(Of ApplicationUser)(New ApplicationDbContext()))
+    End Sub
+End Class
+Public Class IdentityHelper
+    'Used for XSRF when linking external logins
+    Public Const XsrfKey As String = "xsrfKey"
+
+    Public Shared Sub SignIn(manager As UserManager, user As ApplicationUser, isPersistent As Boolean)
+        Dim authenticationManager As IAuthenticationManager = HttpContext.Current.GetOwinContext().Authentication
+        authenticationManager.SignOut(DefaultAuthenticationTypes.ExternalCookie)
+        Dim identity = manager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie)
+        authenticationManager.SignIn(New AuthenticationProperties() With {.IsPersistent = isPersistent}, identity)
+    End Sub
+
+    Public Const ProviderNameKey As String = "providerName"
+    Public Shared Function GetProviderNameFromRequest(request As HttpRequest) As String
+        Return request(ProviderNameKey)
+    End Function
+
+    Private Shared Function IsLocalUrl(url As String) As Boolean
+        Return Not String.IsNullOrEmpty(url) AndAlso ((url(0) = "/"c AndAlso (url.Length = 1 OrElse (url(1) <> "/"c AndAlso url(1) <> "\"c))) OrElse (url.Length > 1 AndAlso url(0) = "~"c AndAlso url(1) = "/"c))
+    End Function
+
 
 
     Public Shared Sub RedirectToReturnUrl(returnUrl As String, response As HttpResponse)
